4af5124e569796ac57441a16d9fffa8f4dbc2f00
OussamaElar <oussama.elar@gmail.com>
2021-11-28 00:00:55 -0500

    4th commit

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 
 
 app.get("/", (req, res) => res.send("fucking work!!"));
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 // middleware for body-parser 
