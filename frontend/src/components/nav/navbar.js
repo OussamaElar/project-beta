088bedf4fc28d2bd0a69516dbd78389534f4edd5
OussamaElar <oussama.elar@gmail.com>
2021-12-01 00:17:13 -0500

    sign up form done

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import './navbar.css'
+import './navbar.css';
+import logo from '../../assets/test1.png'
+import login_form from '../session/login_form';
 
 
 class NavBar extends React.Component {
@@ -17,24 +19,40 @@ class NavBar extends React.Component {
 
       render() {
             return (
-                  <nav className="navbar navbar-inverse">
-                  <div className="container-fluid">
-                        <div className="navbar-header">
-                              <a className="navbar-brand" href="#">ElarMar</a>
-                        </div>
-                        <ul className="nav navbar-nav">
-                              <li className="active"><a href="#">Home</a></li>
-                              <li><a href="#">Profil</a></li>
-                              <li><a href="#">Services</a></li>
+                  // <nav className="navbar navbar-inverse">
+                  // <div className="container-fluid">
+                  //       <div className="navbar-header">
+                  //             <a className="navbar-brand" href="#">ElarMar</a>
+                  //       </div>
+                  //       <ul className="nav navbar-nav">
+                  //             <li className="active"><a href="#">Home</a></li>
+                  //             <li><a href="#">Profil</a></li>
+                  //             <li><a href="#">Services</a></li>
+                  //       </ul>
+                  //       {this.props.loggedIn ? 
+                  //             <button onClick={this.logoutUser} className='btn btn-danger navbar-right logout'>Logout</button>  :
+                  //             <ul className="nav navbar-nav navbar-right">
+                  //                   <li><Link to='/signup' ><span className="glyphicon glyphicon-user"></span> Sign Up</Link></li>
+                  //                   <li><Link to='/login' ><span className="glyphicon glyphicon-log-in"></span> Login</Link></li>
+                  //             </ul>   
+                  //       } 
+                  // </div>
+                  // </nav>
+
+                  <nav className='nav-bar'>
+                        <ul className='nav-bar-l'>
+                              <li><img src={logo} height='40' width='40'/><span className='navbar-menu-item'> ElarMar</span></li>
+                              <li className='navbar-menu-item'><Link to='/'>Home</Link></li>
+                              <li className='navbar-menu-item'>Services</li>
+                              <li className='navbar-menu-item'>About Us</li>
                         </ul>
-                        {this.props.loggedIn ? 
-                              <button onClick={this.logoutUser} className='btn btn-danger navbar-right logout'>Logout</button>  :
-                              <ul className="nav navbar-nav navbar-right">
-                                    <li><Link to='/signup' ><span className="glyphicon glyphicon-user"></span> Sign Up</Link></li>
-                                    <li><Link to='/login' ><span className="glyphicon glyphicon-log-in"></span> Login</Link></li>
-                              </ul>   
-                        } 
-                  </div>
+                              {this.props.loggedIn ? 
+                              <button onClick={this.logoutUser} className='btn btn-danger navbar-right logout'>Logout</button> :
+                              <ul className='nav-bar-r'>
+                                    <li className='navbar-menu-item'><i class="fa fa-sign-in navsupin" aria-hidden="true"></i> <Link to='/login'>Sign In</Link></li>
+                                    <li className='navbar-menu-item'><i class="fa fa-user-plus navsupin" aria-hidden="true"></i> <Link to='/signup'>Sign Up</Link></li>
+                              </ul>
+                              }
                   </nav>
             )
       }
--- frontend/src/components/session/signup_form.js
+++ frontend/src/components/session/signup_form.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import './signup.css';
+import logo from '../../assets/test1.png'
+
 
 class SignupForm extends React.Component {
   constructor(props) {
@@ -47,7 +50,7 @@ class SignupForm extends React.Component {
     return(
       <ul>
         {Object.keys(this.state.errors).map((error, i) => (
-          <li key={`error-${i}`}>
+          <li key={`error-${i}`} className='errors'>
             {this.state.errors[error]}
           </li>
         ))}
@@ -91,106 +94,33 @@ class SignupForm extends React.Component {
       //   </form>
       // </div>
           
-      <section className="vh-100 sign-form" >
-      <div className="container h-100">
-      <div className="row d-flex justify-content-center align-items-center h-100">
-            <div className="col-lg-12 col-xl-11">
-            <div className="card text-black" >
-            <div className="card-body p-md-5">
-                  <div className="row justify-content-center">
-                  <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
-
-                  <p className="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Sign up</p>
-
-                  <form className="mx-1 mx-md-4" onSubmit={this.handleSubmit}>
-
-                        <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                              <input 
-                                    type="text" 
-                                    id="form3Example1c" 
-                                    className="form-control" 
-                                    value={this.state.handle}
-                                    onChange={this.update('handle')}
-                              />
-                              <label className="form-label" for="form3Example1c">Your Name</label>
-                        </div>
-                        </div>
-
-                        <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                              <input 
-                                    type="email" 
-                                    id="form3Example3c" 
-                                    className="form-control" 
-                                    value={this.state.email}
-                                    onChange={this.update('email')}
-                              />
-                              <label className="form-label" for="form3Example3c">Your Email</label>
-                        </div>
-                        </div>
-
-                        <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                              <input 
-                                    type="password" 
-                                    id="form3Example4c" 
-                                    className="form-control"   
-                                    value={this.state.password}
-                                    onChange={this.update('password')}    
-                              />
-                              <label className="form-label" for="form3Example4c">Password</label>
-                        </div>
-                        </div>
-
-                        <div className="d-flex flex-row align-items-center mb-4">
-                        <i className="fas fa-key fa-lg me-3 fa-fw"></i>
-                        <div className="form-outline flex-fill mb-0">
-                              <input 
-                                    type="password" 
-                                    id="form3Example4cd" 
-                                    className="form-control" 
-                                    value={this.state.password2}
-                                    onChange={this.update('password2')}
-                              />
-                              <label className="form-label" for="form3Example4cd">Repeat your password</label>
-                        </div>
-                        </div>
-
-                        <div className="form-check d-flex justify-content-center mb-5">
-                        <input
-                              className="form-check-input me-2"
-                              type="checkbox"
-                              value=""
-                              id="form2Example3c"
-                        />
-                        <label className="form-check-label" for="form2Example3">
-                        I agree all statements in <a href="#!">Terms of service</a>
-                        </label>
-                        </div>
-
-                        <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                        <button type="submit" className="btn btn-primary btn-lg">Register</button>
-                        </div>
-
-                  </form>
-
-                  </div>
-                  <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-
-                  <img src="https://mdbootstrap.com/img/Photos/new-templates/bootstrap-registration/draw1.png" className="img-fluid" alt="Sample image"/>
-
-                  </div>
-                  </div>
-            </div>
-            </div>
-            </div>
-      </div>
-      </div>
-      </section>          
+      <div>
+        <form action="" className='signup-form' onSubmit={this.handleSubmit}>
+          <img src={logo} alt='/' className='signup-logo' />
+          <h4>Please fill out all the fields </h4>
+          {this.renderErrors()}
+          <div className='entry-section'>
+            <label htmlFor='name'>Name</label>
+            <input id='name' type='text' size='40' onChange={this.update('handle')} value={this.state.handle}></input>
+          </div>
+          <div className='entry-section'>
+            <label htmlFor='email'>Email</label>
+            <input id='email' type='email' size='40' onChange={this.update('email')} value={this.state.email} placeholder="Email"></input>
+          </div>
+          <div className='entry-section'>
+            <label htmlFor='pass1'>Password</label>
+            <input id='pass1' type='password' size='40' onChange={this.update('password')} value={this.state.password}></input>
+          </div>
+          <div className='entry-section'>
+            <label htmlFor='pass2'>Repeat Password</label>
+            <input id='pass2' type='password' size='40' onChange={this.update('password2')} value={this.state.password2}></input>
+          </div>
+          <div className='button-section'>
+            <button className='submit-button btn btn-outline-dark'>Register</button>
+            <button className='create-button btn btn-outline-dark'>Login</button>
+          </div>
+        </form>
+      </div>       
     );
   }
 }
