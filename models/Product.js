b5988b652b1edad771d5c949bad12d3ad05490f7
OussamaElar <oussama.elar@gmail.com>
2021-12-21 18:59:06 -0500

    product

---
+++ models/Product.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const ProductSchema = new Schema({
+      user: {
+            type: Schema.Types.ObjectId,
+            ref: 'users',
+            required: true
+      },
+
+      title: {
+            type: String,
+            required: true
+      },
+
+      description: {
+            type: String,
+            required: true
+      },
+
+      price: {
+            type: Number,
+            required: true
+      },
+
+      productImage: {
+            type: String
+      },
+
+      date: {
+            type: Date
+      }
+      
+},{
+            timestamps: true
+})
