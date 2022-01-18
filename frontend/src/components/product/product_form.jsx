c5f150687c75cb9642c26ecb1bbd63dc1acf3cb9
OussamaElar <oussama.elar@gmail.com>
2022-01-17 21:11:15 -0500

    create product

--- frontend/src/components/product/product_form.jsx
+++ frontend/src/components/product/product_form.jsx
@@ -3,19 +3,29 @@ import './produc_create.css';
 
 
 const ProductCreate = (props) => {
+      const [values, setValues] = useState(props.product);
+      const handleChange = (field) => {
+            return e => {
+                  e.persist();
+                  setValues((values) => ({
+                        ...values,
+                        [field]: e.target.value
+                  }))
+            }
+      }
       return (
             <form className='create_form'>
                   <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Title</label>
-                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" value={values.title}/>
                   </div>
                   <div class="mb-3">
                         <label for="exampleFormControlTextarea1" class="form-label">Description</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" value={values.description}></textarea>
                   </div>
                   <div class="mb-3">
                         <label for="exampleFormControlInput1" class="form-label">Price</label>
-                        <input type="number" class="form-control" id="exampleFormControlInput2" placeholder="Price"/>
+                        <input type="number" class="form-control" id="exampleFormControlInput2" placeholder="Price" value={ values.price }/>
                   </div>
                   <button type="submit" className="btn btn-warning">Submit</button>                  
             </form>
