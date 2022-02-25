0284e47a360855d7a4c9285d699b5bf80dffe9f5
OussamaElar <oussama.elar@gmail.com>
2022-02-25 16:53:58 -0500

    product show

---
+++ frontend/src/components/product/product_show_container.js
@@ -0,0 +1,21 @@
+import { connect } from "react-redux";
+import { deleteProduct, fetchProducts } from "../../actions/product_action";
+import ProductShow from './product_show';
+
+
+
+const mSTP = (state) => {
+      return {
+            products: Object.values(state.products.all),
+            userId: state.session.user.id
+      }
+}
+
+const mDTP = (dispatch) => {
+      return {
+            fetchProducts: () => dispatch(fetchProducts()),
+            deleteProduct: (productId) => dispatch(deleteProduct(productId))
+      }
+}
+
+export default connect(mSTP, mDTP)(ProductShow);
