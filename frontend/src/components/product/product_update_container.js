5d8afd682c989ef80de2a8edeec00c836f314fd0
OussamaElar <oussama.elar@gmail.com>
2022-03-21 18:22:48 -0400

    update bug

--- frontend/src/components/product/product_update_container.js
+++ frontend/src/components/product/product_update_container.js
@@ -6,7 +6,7 @@ import ProductCreate from './product_form';
 
 // finish first the product show and add button for update product
 // product update has a problem on updateing the actual product 
-// today i haven't done notthing 
+// still working on the update bug 
 const EditProduct = (props) => {
       useEffect(() => {
             props.fetchProduct(props.match.params.id)
